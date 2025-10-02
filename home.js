a507205d7a0771ca5c2e5c71af7ea15e679bbe81
Yogesh Ahire <yogeshpahire05@gmail,com>
2025-10-02 17:15:25 +0530

    resonsive features

--- home.js
+++ home.js
@@ -6,13 +6,17 @@ const welcomeText = document.getElementById("welcomeText");
 if (username) {
   welcomeText.textContent = `Hi ${username}, Ready for the Quiz? 🎯`;
 } else {
-  // If no login → go back
   window.location.href = "login.html";
 }
 
 // ✅ Logout Button
 const logoutBtn = document.getElementById("logoutBtn");
 logoutBtn.addEventListener("click", () => {
-  localStorage.removeItem("quizUsername"); // clear login
-  window.location.href = "login.html";     // back to login
+  localStorage.removeItem("quizUsername");
+  window.location.href = "login.html";
+});
+
+// ✅ Mobile Menu Toggle
+document.getElementById("menu-toggle").addEventListener("click", () => {
+  document.getElementById("nav-links").classList.toggle("active");
 });
